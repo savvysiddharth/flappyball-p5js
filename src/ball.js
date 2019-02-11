35861a53f1b5048a63d72958371dd42cfbb29b86
savvysiddharth <siddharth21805@gmail.com>
2019-02-11 21:06:17 +0530

    code indentation fixed

--- src/ball.js
+++ src/ball.js
@@ -1,56 +1,53 @@
 class Ball
 {
-	constructor(world)
-	{
-		this.world = world;
-		this.x = this.world.width / 2; //always stays on center of x-axis
-		this.y = this.world.height / 2;
-		this.speed = 0; //normal speed of moving w/o external force in y-axis
-		this.diameter = 30;
-		this.radius = this.diameter / 2;
+  constructor(world) {
+    this.world = world;
+    this.x = this.world.width / 2; //always stays on center of x-axis
+    this.y = this.world.height / 2;
+    this.speed = 0; //normal speed of moving w/o external force in y-axis
+    this.diameter = 30;
+    this.radius = this.diameter / 2;
 
-		this.thrust = -7; //thrust up while jumping
+    this.thrust = -7; //thrust up while jumping
 
-		this.score = 0;
+    this.score = 0;
 
-		this.img = ballimg;
-	}
+    this.img = ballimg;
+  }
 
-	applyWorldEffects() //apply gravity and stop at ground
-	{
-		if(this.y >= this.world.height - 10) //on ground!
-		{
-			this.speed = -5; //reversing by small amt
-		}
-		else if(this.y < 0) //going over roof
-		{
-			this.speed = 1; //reversing by small amt
-		}
-		else //falling
-		{
-			this.speed=this.speed + this.world.gravity; //gravity = 0.5
-		}
-	}
+  applyWorldEffects() //apply gravity and stop at ground
+  {
+    if(this.y >= this.world.height - 10) //on ground!
+    {
+      this.speed = -5; //reversing by small amt
+    }
+    else if(this.y < 0) //going over roof
+    {
+      this.speed = 1; //reversing by small amt
+    }
+    else //falling
+    {
+      this.speed=this.speed + this.world.gravity; //gravity = 0.5
+    }
+  }
 
-	jump() {
-		this.speed = this.thrust;
-	}
+  jump() {
+    this.speed = this.thrust;
+  }
 
-	keyCheck()
-	{
-		if(keyIsPressed || mouseIsPressed) {
-			this.speed = this.speed - this.thrust;
-		}
-	}
+  keyCheck() {
+    if(keyIsPressed || mouseIsPressed) {
+      this.speed = this.speed - this.thrust;
+    }
+  }
 
-	display()
-	{
-		fill(255); //white color
-		image(this.img, this.x - this.radius, this.y - this.radius ,this.diameter+5,this.diameter+5);
-		// ellipse(this.x , this.y , this.diameter , this.diameter); //to display circle
-	}
+  display() {
+    fill(255); //white color
+    image(this.img, this.x - this.radius, this.y - this.radius ,this.diameter+5,this.diameter+5);
+    // ellipse(this.x , this.y , this.diameter , this.diameter); //to display circle
+  }
 
-	move() {
-		this.y = this.y + this.speed;
-	}
+  move() {
+    this.y = this.y + this.speed;
+  }
 }
--- src/sketch.js
+++ src/sketch.js
@@ -1,58 +1,58 @@
 class Scoreboard {
-	constructor() {
-		this.score = 0;
+  constructor() {
+    this.score = 0;
 
-		this.box1 = document.getElementById('score');
-		this.box1.innerHTML = "Score : "+this.score;
+    this.box1 = document.getElementById('score');
+    this.box1.innerHTML = "Score : "+this.score;
 
-		this.history = [0];
+    this.history = [0];
 
-		this.box2 = document.getElementById('hscore');
-		this.box2.innerHTML = "High Score : "+this.highScore();
-	}
+    this.box2 = document.getElementById('hscore');
+    this.box2.innerHTML = "High Score : "+this.highScore();
+  }
 
-	update() {
-		this.box1.innerHTML = "Score : "+this.score;
-	}
+  update() {
+    this.box1.innerHTML = "Score : "+this.score;
+  }
 
-	resetScore() {
-		this.history.push(this.score);
-		this.score = 0;
-		this.update();
-		this.updateHighScore();
-	}
+  resetScore() {
+    this.history.push(this.score);
+    this.score = 0;
+    this.update();
+    this.updateHighScore();
+  }
 
-	updateHighScore() {
-		this.box2.innerHTML = "High Score : "+this.highScore();
-		this.history = [this.highScore()];
-	}
+  updateHighScore() {
+    this.box2.innerHTML = "High Score : "+this.highScore();
+    this.history = [this.highScore()];
+  }
 
-	highScore() {
-		let max = this.history[0];
-		for(let score of this.history) {
-			if(score > max)
-				max = score;
-		}
-		return max;
-	}
+  highScore() {
+    let max = this.history[0];
+    for (let score of this.history) {
+      if (score > max)
+        max = score;
+    }
+    return max;
+  }
 }
 
 let world;
 let scoreboard;
 
 function preload() {
-	console.log('helop');
-	ballimg = loadImage('ball5.png')
-	bgImg = loadImage('bg5.jpg');
-	barImg = loadImage('wood2.jpeg');
+  console.log('helop');
+  ballimg = loadImage('ball5.png')
+  bgImg = loadImage('bg5.jpg');
+  barImg = loadImage('wood2.jpeg');
 }
 
 function setup() {
-	scoreboard = new Scoreboard();
-	world = new World(700,500);
+  scoreboard = new Scoreboard();
+  world = new World(700,500);
 
-	createCanvas(world.width,world.height);
-	background(0);
+  createCanvas(world.width,world.height);
+  background(0);
 }
 
 let bgX = 0;
@@ -64,8 +64,8 @@ let firstStart = true;
 collisionDetection = true;
 
 function draw() {
-	background(0);
-	image(bgImg, bgX, 0, bgImg.width, height);
+  background(0);
+  image(bgImg, bgX, 0, bgImg.width, height);
     bgX -= world.speed * parallax;
 
 
@@ -73,8 +73,8 @@ function draw() {
     // edge of the image would be on the screen, if it is draw a
     // second copy of the image right next to it
     // once the second image gets to the 0 point, we can reset bgX to
-	// 0 and go back to drawing just one image.
-	//Orignal code from - https://codingtrain.github.io/Flappy-Bird-Clone/sketch.js (line 52-57)
+  // 0 and go back to drawing just one image.
+  //Orignal code from - https://codingtrain.github.io/Flappy-Bird-Clone/sketch.js (line 52-57)
     if (bgX <= -bgImg.width + width) {
       image(bgImg, bgX + bgImg.width, 0, bgImg.width, height);
     if (bgX <= -bgImg.width) {
@@ -83,70 +83,70 @@ function draw() {
     }
 
 
-	//BALL
-	world.ball.applyWorldEffects();
-	world.ball.move();
-	world.ball.display();
+  //BALL
+  world.ball.applyWorldEffects();
+  world.ball.move();
+  world.ball.display();
 
 
-	//BARS
-	if(world.isLastBarGone())
-		world.resetBars(); //reset all bars
-	for(let i=0;i<world.totalBars;i++) {
-		world.bar[i].move();
-		world.bar[i].display();
-		let justmarked = world.bar[i].markIfVisited(world.ball.diameter);
-		if(justmarked) {
-			world.ball.score++;
-			scoreboard.score = world.ball.score;
-			scoreboard.update();
-		}
-		if(!world.bar[i].marked){
-			// console.log('checking collision.');
-			if(world.barBallCollision(world.bar[i]) && collisionDetection ) {
-				gamePause = true;
-				textSize(62);
-				fill(255);
-				textAlign(CENTER);
-				textStyle(BOLD);
-				strokeWeight(5);
-				stroke(0);
-				text('GAME OVER',world.width/2,world.height/2);
-				textSize(16);
-				text('press space to start again',world.width/2,world.height/2+50);
-				console.log('oops');
-				noLoop();
+  //BARS
+  if (world.isLastBarGone())
+    world.resetBars(); //reset all bars
+  for (let i=0;i<world.totalBars;i++) {
+    world.bar[i].move();
+    world.bar[i].display();
+    let justmarked = world.bar[i].markIfVisited(world.ball.diameter);
+    if (justmarked) {
+      world.ball.score++;
+      scoreboard.score = world.ball.score;
+      scoreboard.update();
+    }
+    if (!world.bar[i].marked) {
+      // console.log('checking collision.');
+      if (world.barBallCollision(world.bar[i]) && collisionDetection ) {
+        gamePause = true;
+        textSize(62);
+        fill(255);
+        textAlign(CENTER);
+        textStyle(BOLD);
+        strokeWeight(5);
+        stroke(0);
+        text('GAME OVER',world.width/2,world.height/2);
+        textSize(16);
+        text('press space to start again',world.width/2,world.height/2+50);
+        console.log('oops');
+        noLoop();
 
-				world.resetBars();
-				world.ball.score = 0
-				scoreboard.resetScore();
-				break;
-			}
-		}
-	}
+        world.resetBars();
+        world.ball.score = 0
+        scoreboard.resetScore();
+        break;
+      }
+    }
+  }
 
-	if(firstStart) {
-		textSize(62);
-		fill(255);
-		textAlign(CENTER);
-		textStyle(BOLD);
-		strokeWeight(10);
-		stroke(0);
-		text('flappyBird clone',world.width/2,world.height/2);
-		textSize(16);
-		text('press any key to start',world.width/2,world.height/2+50);
-		noLoop();
-	}
+  if(firstStart) {
+    textSize(62);
+    fill(255);
+    textAlign(CENTER);
+    textStyle(BOLD);
+    strokeWeight(10);
+    stroke(0);
+    text('flappyBird clone',world.width/2,world.height/2);
+    textSize(16);
+    text('press any key to start',world.width/2,world.height/2+50);
+    noLoop();
+  }
 }
 
 function keyPressed() {
-	world.ball.jump();
-	if(firstStart) {
-		firstStart = false;
-		loop();
-	}
-	if(gamePause && !firstStart && key==' ') {
-		gamePause=false;
-		loop();
-	}
+  world.ball.jump();
+  if(firstStart) {
+    firstStart = false;
+    loop();
+  }
+  if(gamePause && !firstStart && key==' ') {
+    gamePause=false;
+    loop();
+  }
 }
